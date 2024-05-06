d0d38d8ea20e78774bed49ada6a8e33cf727b0b0
Daniel <danielfcg4@gmail.com>
2024-05-05 22:29:03 -0500

    ...

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,webp}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "bg-main": "url('src/assets/wisky2.webp')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
