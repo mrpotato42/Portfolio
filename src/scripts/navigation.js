df376d952f7dcfe0e2afaf92de3d03d7065d9e64
Daniel <danielfcg4@gmail.com>
2024-05-12 17:01:56 -0500

    Added Preact to Astro

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,39 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuButton = document.querySelector("#menu-button");
-  const mobileMenu = document.querySelector("#mobile-menu");
-  const divButton = document.querySelector("#div-button");
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('[title]');
 
-  // Función para cerrar el menú
-  function closeMenu() {
-    mobileMenu.classList.add("hidden");
-    divButton.classList.toggle("hover:to-transparent");
-    divButton.classList.toggle("hover:bg-gradient-to-t");
-    menuButton.classList.toggle("hover:shadow");
-  }
-
-  function openMenu() {
-    mobileMenu.classList.remove("hidden");
-    divButton.classList.toggle("hover:to-transparent");
-    divButton.classList.toggle("hover:bg-gradient-to-t");
-    menuButton.classList.toggle("hover:shadow");
-  }
-  // Event listener para el botón del menú
-  menuButton.addEventListener("click", function (event) {
-    event.stopPropagation(); // Evita que el evento se propague al documento
-    const isMenuOpen = mobileMenu.classList.contains("hidden");
-    if (isMenuOpen) {
-      openMenu();
-    } else {
-      closeMenu();
-    }
-  });
-
-  // Event listener para el documento
-  document.addEventListener("click", function (event) {
-    // Verifica si el clic fue dentro del menú
-    if (!mobileMenu.contains(event.target)) {
-      // Si el clic fue fuera del menú, cierra el menú
-      closeMenu();
-    }
+  buttons.forEach(button => {
+    button.addEventListener('click', function() {
+      const url = this.getAttribute('title');
+      console.log(`Botón con URL ${url} fue presionado.`);
+      // Almacena el URL en el almacenamiento local
+      localStorage.setItem('url', url);
+    });
   });
 });
